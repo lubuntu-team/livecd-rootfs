577bdaf2592db9104f8f08408dc06f87f09cc1b9
Steve Langasek <steve.langasek@ubuntu.com>
2008-02-14 10:15:33 -0800

    Import patches-unapplied version 0.51 to ubuntu/hardy
    Imported using git-ubuntu import.
    
    Changelog parent: 7bb17dadb64a96f21e5f4fe9346734088b39b7de
    
    New changelog entries:
      [ Jonathan Riddell ]
      * Add kubuntu-kde4
      [ Steve Langasek ]
      * Add new targets for {k,ed,}ubuntu-dvd

--- livecd.sh
+++ livecd.sh
@@ -129,15 +129,15 @@ esac; done;
 shift $((OPTIND-1))
 
 if (( $# == 0 )) || [ "X$1" = "Xall" ]; then
-    set -- ubuntu kubuntu edubuntu xubuntu gobuntu base
+    set -- ubuntu kubuntu kubuntu-kde4 edubuntu xubuntu gobuntu base
     if [ "$ARCH" = "i386" ]; then
-        set -- ubuntu ubuntu-lpia kubuntu edubuntu xubuntu gobuntu base
+        set -- ubuntu ubuntu-dvd ubuntu-lpia kubuntu kubuntu-dvd kubuntu-kde4 edubuntu edubuntu-dvd xubuntu gobuntu base
     fi
 fi
 
 for arg in "$@"; do
     case "$arg" in
-	ubuntu|ubuntu-lpia|edubuntu|kubuntu|xubuntu|gobuntu|base|tocd)
+	ubuntu|ubuntu-dvd|ubuntu-lpia|edubuntu|edubuntu-dvd|kubuntu|kubuntu-dvd|kubuntu-kde4|xubuntu|gobuntu|base|tocd)
 	    ;;
 	*)
 	    echo bad name >&2;
@@ -169,15 +169,19 @@ Flags: seen
 @@EOF
 
     case "$FS" in
-	ubuntu|ubuntu-lpia)
+	ubuntu|ubuntu-lpia|ubuntu-dvd)
 	    LIST="$LIST minimal^ standard^ ubuntu-desktop^"
 	    LIVELIST="ubuntu-live^ xresprobe laptop-detect casper lupin-casper"
 	    ;;
-	kubuntu)
+	kubuntu|kubuntu-dvd)
 	    LIST="$LIST minimal^ standard^ kubuntu-desktop^"
 	    LIVELIST="kubuntu-live^ xresprobe laptop-detect casper lupin-casper"
 	    ;;
-	edubuntu)
+	kubuntu-kde4)
+	    LIST="$LIST minimal^ standard^ kubuntu-kde4-desktop^"
+	    LIVELIST="kubuntu-kde4-live^ xresprobe laptop-detect casper lupin-casper"
+	    ;;
+	edubuntu|edubuntu-dvd)
 	    LIST="$LIST minimal^ standard^ edubuntu-desktop^"
 	    LIVELIST="edubuntu-live^ xresprobe laptop-detect casper lupin-casper"
 	    ;;
@@ -218,6 +222,11 @@ Flags: seen
 	    [ -d "$tocdtmp" ] && rm -rf "$tocdtmp"
 	    LIST="$LIST $tocddesktop"
 	    LIVELIST="$tocdlive casper"
+    esac
+    case "$FS" in
+	*-dvd)
+	    LIVELIST="$LIVELIST ${FS}-live^"
+	    ;;
     esac
 
     dpkg -l livecd-rootfs || true	# get our version # in the log.
