2181c3bb4a4213df852ef92d4bde03656fc403cc
Colin Watson <cjwatson@ubuntu.com>
2008-07-18 20:01:15 +0100

    Import patches-unapplied version 0.62 to ubuntu/intrepid
    Imported using git-ubuntu import.
    
    Changelog parent: a754f6c1db606320c472ba8b8a3c807a2cf1c14a
    
    New changelog entries:
      * Use 'set -eu' rather than putting -eu on the #! line, so that running
        'sh -x /usr/sbin/livecd.sh' for testing doesn't invoke different
        error-handling behaviour.
      * Remove the kernel from the livefs; ubiquity >= 1.9.4 will copy it from
        the CD root if it needs to (LP: #80385).

--- livecd.sh
+++ livecd.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -eu
+#!/bin/bash
+set -eu
 
 ##########################################################################
 ####           (c) Copyright 2004-2007 Canonical Ltd.                #####
@@ -430,7 +431,9 @@ deb-src ${USERMIRROR} ${STE}-updates ${COMP}
 	# we mv the initramfs, so it's not wasting space on the livefs
 	mv ${ROOT}/boot/initrd.img-"${KVER}" livecd.${FSS}.initrd-"${SUBARCH}"
 	rm -f ${ROOT}/boot/initrd.img-"${KVER}".bak
-	cp ${ROOT}/boot/vmlinu?-"${KVER}" livecd.${FSS}.kernel-"${SUBARCH}"
+	# ubiquity >= 1.9.4 copies the kernel from the CD root if it doesn't
+	# find one on the livefs, allowing us to save space
+	mv ${ROOT}/boot/vmlinu?-"${KVER}" livecd.${FSS}.kernel-"${SUBARCH}"
     done
     NUMKVERS="$(set -- $KVERS; echo $#)"
     if [ "$NUMKVERS" = 1 ]; then
