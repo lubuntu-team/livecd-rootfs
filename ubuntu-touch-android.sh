fdf09ef826d9f9e4c3a9fa8e509a40bafe98e579
Oliver Grawert <ogra@ubuntu.com>
2013-04-16 11:07:44 +0200

    Import patches-unapplied version 2.123 to ubuntu/raring-proposed
    Imported using git-ubuntu import.
    
    Changelog parent: 19d6263b9983ebc28bd7506d7c0898188df17ccc
    
    New changelog entries:
      * fix typos in debootstrap line of ubuntu-touch-android.sh
      * redo ubuntu-touch-android.sh to use its own build chroot since we can not
        add universe support to the livefs builder chroot
      * ubuntu-touch-android.sh: enable universe in the build chroot

--- ubuntu-touch-android.sh
+++ ubuntu-touch-android.sh
@@ -1,36 +1,62 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 codename=$1
 builddir=$codename-build
+release=raring
 
 [ "$(dpkg --print-architecture)" = "amd64" ] || exit 1
 
-# set up multiarch
-dpkg --print-foreign-architectures | grep -q i386 || dpkg --add-architecture i386
-apt-get update
+# set up a build chroot
+case $(hostname --fqdn) in
+	*.buildd)
+		MIRROR=http://ftpmaster.internal/ubuntu
+		;;
+	*)
+		MIRROR=http://archive.ubuntu.com/ubuntu
+		;;
+esac
+debootstrap --components=main,universe $release $builddir $MIRROR
+
+mount -t devpts devpts-$builddir $builddir/dev/pts
+chroot mount -t proc proc-$builddir /proc
+chroot mount -t sysfs sys-$builddir /sys
+
+# set up multiarch inside the chroot
+chroot $builddir dpkg --add-architecture i386
+chroot $builddir apt-get update
 
 # add cross build env including the needed i386 packages
-apt-get -y install git gnupg flex bison gperf build-essential \
+chroot $builddir apt-get -y install git gnupg flex bison gperf build-essential \
     zip bzr curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
     libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
     libgl1-mesa-dev g++-multilib mingw32 tofrodos phablet-tools \
     python-markdown libxml2-utils xsltproc zlib1g-dev:i386 schedtool \
 	openjdk-6-jdk
 
-# get the git tree
-phablet-dev-bootstrap -v $codename $builddir
+# create an in chroot script to get the git tree and build it
+cat << 'EOF' > $builddir/build-android.sh
+#!/bin/bash
 
+phablet-dev-bootstrap -v $codename $builddir
 cd $builddir
 repo sync
-
 . build/envsetup.sh
 brunch $codename
+EOF
 
-cd -
-cp $builddir/out/target/product/$codename/*-$codename.zip ./livecd.ubuntu-touch-$codename.zip
+chmod +x $builddir/build-android.sh
+
+chroot $builddir /build-android.sh
+
+cp $builddir/$builddir/out/target/product/$codename/*-$codename.zip ./livecd.ubuntu-touch-$codename.zip
 for image in system recovery boot; do
-	cp $builddir/out/target/product/$codename/$image.img ./livecd.ubuntu-touch-$codename.$image.img
+	cp $builddir/$builddir/out/target/product/$codename/$image.img ./livecd.ubuntu-touch-$codename.$image.img
 done
-rm -rf $buildir
+
+umount $builddir/sys
+umount $builddir/proc
+umount $builddir/dev/pts
+
+rm -rf $builddir
