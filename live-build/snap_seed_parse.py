3c3be06f27b324286bfc4d5236f677dc5c074561
Cody Shepherd <cody.shepherd@canonical.com>
2018-10-16 14:50:14 -0700

    Improved logging, comments, and fixed undefined shell variable error.

--- live-build/snap_seed_parse.py
+++ live-build/snap_seed_parse.py
@@ -20,30 +20,33 @@ log("Parsing seed.yaml")
 CHROOT_ROOT = sys.argv[1] if len(sys.argv) > 1 and len(sys.argv[1]) > 0 \
                           else ''
 '''The chroot rooth path should be passed in by the caller'''
-log("CHROOT_ROOT: {}".format(CHROOT_ROOT))
 
+log("CHROOT_ROOT: {}".format(CHROOT_ROOT))
 if len(CHROOT_ROOT) > 0 and CHROOT_ROOT[-1] == '/':
     CHROOT_ROOT = CHROOT_ROOT[:-1]
+'''Trim any trailing slashes for correct appending'''
+
 YAML_PATH = CHROOT_ROOT + '/var/lib/snapd/seed/seed.yaml'
 '''This is where we expect to find the seed.yaml file'''
+
 LINE_PREFIX = 'snap:'
 '''Snaps are prepended with this string in the manifest'''
 
 log("yaml path: {}".format(YAML_PATH))
 if not os.path.isfile(YAML_PATH):
-    log("yaml path not found.")
-    exit(1)
+    '''Exit gracefully and issue a warning if seeded snaps not found'''
+    log("WARNING: yaml path not found; no seeded snaps found.")
+    exit(0)
 else:
     log("yaml path found.")
 
-# Open the seed.yaml and ingest its contents
 with open(YAML_PATH, 'r') as fh:
+    '''Open the seed.yaml and ingest its contents'''
     yaml_lines = yaml.load(fh)['snaps']
 
 to_write = ''
-# Loop over dict items, generating one manifest line from each
-# triplet
 for item in yaml_lines:
+    '''Loop over dict items, generating one manifest line from each triplet'''
     filestring = item['file']
     revision = filestring[filestring.rindex('_')+1:]
     revision = re.sub(r'[^0-9]', '', revision)
@@ -53,5 +56,5 @@ for item in yaml_lines:
                                         revision,
                                         )
 
-# write accumulated text to stdout for redirection
 print(to_write.strip('\n'))
+'''write accumulated text to stdout for redirection'''
